ce9e328d0b36b87e1e1dfaaa8fd42b4ca1828696
Manish <shubham.bajpai@xelpmoc.in>
2017-12-08 11:27:05 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <footer>Hellooo React</footer>
       </div>
     );
   }
