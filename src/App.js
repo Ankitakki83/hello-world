6455429c560836f78632e63d8dee8e24ab0c30f9
Manish <shubham.bajpai@xelpmoc.in>
2017-12-08 15:24:24 +0530

    Adding again

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let txt=this.props.txt
     return (
       <div className="App">
         <header className="App-header">
-        <center>React JS</center>
+        <center> <h1>  React JS  </h1> </center>
         </header>
+
+        <body  bgcolor="skyblue">
+          <div >
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+         <h1>Simple text h1</h1>
+         <h2>Simple text h1</h2>
+         
+         <h1>{txt}</h1>
+         </div>
 
-        <footer> The footer of React JS </footer>
+         <div >
+
+           <h3> simple text h3 </h3>
+
+           </div>
+        </body>
+        <footer> The footer of React JS </footer> 
       </div>
     );
   }
